097018627ed2af5ac15487dca4c431978b12fa53
vara <vara@outlook.com>
2017-11-10 08:22:06 +0800

    Add simple line chart

--- src/barChart.js
+++ src/barChart.js
@@ -1,42 +1,57 @@
 import * as d3 from 'd3';
 require("./styles/barChart.styl")
-var data = require('./data/barChart.tsv')
-var width = 420,
-    barHeight = 20;
+var data = require('./data/barChart.csv')
 
-var x = d3.scaleLinear()
-    .range([0, width]);
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-var chart = d3.select(".chart")
-    .attr("width", width);
+// parse the date / time
+var parseTime = d3.timeParse("%d-%b-%y");
 
-d3.tsv("./data/barChart.tsv", function(error, data){
-  // console.log(data);
-})
+// set the ranges
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
 
-data.map( (item,index) => {
-console.log(item["name value"] );
-  x.domain([0, d3.max(data, function(d) { return d.value; })]);
+// define the line
+var valueline = d3.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.close); });
 
-  chart.attr("height", barHeight * data.length);
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-  var bar = chart.selectAll("g")
-      .data(data)
-      .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+// Get the data
 
-  bar.append("rect")
-      .attr("width", function(item) { return x(item.value); })
-      .attr("height", barHeight - 1);
+console.log(data[0]);
+  // format the data
+  data.forEach(function(d) {
+      d.date = parseTime(d.date);
+      d.close = +d.close;
+  });
 
-  bar.append("text")
-      .attr("x", function(d) { return x(d.value) - 3; })
-      .attr("y", barHeight / 2)
-      .attr("dy", ".35em")
-      .text(function(data) { return data.value; });
-});
+  // Scale the range of the data
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain([0, d3.max(data, function(d) { return d.close; })]);
 
-function type(d) {
-  d.value = +d.value; // coerce to number
-  return d;
-}
+  // Add the valueline path.
+  svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .attr("d", valueline);
+
+  // Add the X Axis
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  // Add the Y Axis
+  svg.append("g")
+      .call(d3.axisLeft(y));
